e73597e5861c7d58eb0b5a2c8ac7f8fc3a8a06b8
Ivan kovacic <ivan.kovacic@fiveminutes.eu>
2018-01-29 15:30:46 +0100

    init files

---
+++ AngularFrontEnd/Scripts/Script.js
@@ -0,0 +1,9 @@
+﻿
+
+var testApp = angular.module("makeModule", [])
+    .controller("makeController", function ($scope, $http) {
+    $http.get('http://localhost:56543/api/make').then(function (response) {
+        debugger;
+            $scope.make = response.data.item;
+        });
+    });
